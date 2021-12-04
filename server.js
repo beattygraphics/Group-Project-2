0b80f28f718f2b830dbf43df9f4bc3120ddc3f9d
Omar Pacheco <omarspacheco@gmail.com>
2021-12-04 11:39:13 -0500

    Session prep

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -11,18 +11,17 @@ const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Session stuff that will be used later
-// const sess = {
-//     secret: 'Super secret secret', // TODO: Lookup a package that can handle random secret gen
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize,
-//     }),
-// };
+const sess = {
+    secret: 'Han shot first',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
@@ -35,11 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// Use once sequelize is set up
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
-// app.listen(PORT, () =>
-//     console.log(`Server running on port ${PORT}. Visit http://localhost:${PORT} now!`)
-// );
