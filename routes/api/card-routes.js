341e883f05320dba477a949edbf41d1116cfb32a
Josh Elrod <elrodjosh96@gmail.com>
2021-12-04 10:48:42 -0500

    updated routes

--- routes/api/card-routes.js
+++ routes/api/card-routes.js
@@ -5,11 +5,25 @@ const { Card, Favorites } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all cards by category
-
+  try {
+    const categoryData = await Category.findAll({include: [{model: Card}]});
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find cards by its Favorites value
+  try {
+    const categoryData = await Favorites.findByPk(req.params.id, {include: [{model: Card}]});
+    res.status(200).json(categoryData)
+    if (!categoryData) {
+      res.status(404).json({message: 'Cannot find category with this ID'})
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 });
 
--- routes/api/categories -routes.js
+++ routes/api/categories -routes.js
@@ -3,13 +3,28 @@ const { Category, Card, Favorites } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll({include: [{model: Card}]});
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {include: [{model: Card}]});
+    res.status(200).json(categoryData)
+    if (!categoryData) {
+      res.status(404).json({message: 'Cannot find category with this ID'})
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 });
 
