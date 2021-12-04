cfaacf79b513f0a62526620fdc52c1a11e09da43
Omar Pacheco <omarspacheco@gmail.com>
2021-12-04 13:22:00 -0500

    auth work continued

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const cards = require('./card');
 const categories = require('./categories');
 const favorites = require('./favorites');
+const userRoutes = require('./userRoutes');
 
 router.use('/card', cards);
 router.use('/categoires', categories);
 router.use('/favorites', favorites);
+router.use('/user', userRoutes);
 
 module.exports = router;
